5e3f7bb010da19dc6bbe802d78072d24a50b1593
Nicolas Torres <Nicolas.torres-p@mail.escuelaing.edu.co>
2021-08-12 10:35:40 -0500

    Deploy Heroku

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,56 @@
 package org.example;
 
+
+
+import spark.Request;
+import spark.Response;
+import static spark.Spark.*;
+
 /**
- * Hello world!
+ * Minimal web app example for Heroku using SparkWeb
  *
+ * @author daniel
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    /**
+     * This main method uses SparkWeb static methods and lambda functions to
+     * create a simple Hello World web app. It maps the lambda function to the
+     * /hello relative URL.
+     */
+    public static void main(String[] args) {
+        // root is 'src/main/resources', so put files in 'src/main/resources/public'
+        staticFiles.location("/public");
+        port(getPort());
+        //get("/inputdata", (req, res) -> inputDataPage(req, res));
+        get("/results", (req, res) -> resultsPage(req, res));
     }
+
+    /**private static String inputDataPage(Request req, Response res) {
+        String pageContent
+
+        return pageContent;
+    }**/
+
+    private static String resultsPage(Request req, Response res) {
+        return req.queryParams("firstname") + " " +
+                req.queryParams("lastname");
+    }
+
+    /**
+     * This method reads the default port as specified by the PORT variable in
+     * the environment.
+     *
+     * Heroku provides the port automatically so you need this to run the
+     * project on Heroku.
+     */
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567; //returns default port if heroku-port isn't set (i.e. on localhost)
+    }
+
 }
+
+
